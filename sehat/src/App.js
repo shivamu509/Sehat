51c342a393b2c46f40791319bb1495fc561e731f
SambitKumarNanda <nanu2647542@gmail.com>
2022-09-10 11:14:45 +0530

    homepage and navbar added

--- sehat/src/App.js
+++ sehat/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/HomePage/Navbar";
+import HomePage from "./components/HomePage/HomePage";
 import HeroSection from "./components/HospitalList/HeroSection";
 
 function App() {
-  
-  return <div>
-    <HeroSection />
-  </div>
-  
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/find-hospital" element={<HeroSection />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
