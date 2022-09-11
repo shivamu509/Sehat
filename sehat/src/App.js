be20f43e5bb3e71ee9fbd1028abe6fb06ef775a0
Shivam <shivamu509@gmail.com>
2022-09-11 12:23:21 +0530

    added hospital details form

--- sehat/src/App.js
+++ sehat/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/HomePage/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import HospitalDetails from "./components/HospitalDetails/Details";
 import FindHospital from "./components/HospitalList/HeroSection";
+import AppointmentDetails from "./components/appointmentDetails/AppointmentDetails";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/find-hospital" element={<FindHospital />} />
+        <Route path="/appointment/details" element={<AppointmentDetails />} />
         <Route path="/find-hospital/details" element={<HospitalDetails />} />
       </Routes>
     </Router>
