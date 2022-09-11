152df0b87213de867242ef53bf8e276ec2a4e94a
ParadoxPD <ask.priyanshu@gmail.com>
2022-09-11 09:09:45 +0530

    Added Deployment

--- sehat-backend/app.js
+++ sehat-backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("api/hospital", hospitalRoutes);
+app.use("/api/hospital", hospitalRoutes);
 app.use("/api/user", userRoutes);
 
 dbConn();
